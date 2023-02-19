48ddb1f430dfeadea7a96abde11673f1bd7363b1
HUAIJIE0314 <60058096+HUAIJIE0314@users.noreply.github.com>
2023-02-20 02:49:02 +0800

    Update LCD_CTRL.v

--- 2019_E_pre/src/LCD_CTRL.v
+++ 2019_E_pre/src/LCD_CTRL.v
@@ -21,21 +21,21 @@ input              reset;
 input  [3:0]         cmd;
 input          cmd_valid;
 input  [7:0]      IROM_Q;
-output           IROM_rd;//
-output [5:0]      IROM_A;//
-output        IRAM_valid;//
-output [7:0]      IRAM_D;//
-output [5:0]      IRAM_A;//
-output              busy;//
-output              done;//
+output           IROM_rd;
+output [5:0]      IROM_A;
+output        IRAM_valid;
+output [7:0]      IRAM_D;
+output [5:0]      IRAM_A;
+output              busy;
+output              done;
  
-reg              IROM_rd;//
-reg    [5:0]      IROM_A;//
-reg    [7:0]      IRAM_D;//
-reg    [5:0]      IRAM_A;//
-reg           IRAM_valid;//
-reg                 busy;//
-reg                 done;//
+reg              IROM_rd;
+reg    [5:0]      IROM_A;
+reg    [7:0]      IRAM_D;
+reg    [5:0]      IRAM_A;
+reg           IRAM_valid;
+reg                 busy;
+reg                 done;
 //---------------------------------------------------------------------
 //   LOCAL PARAMETER                       
 //--------------------------------------------------------------------- 
